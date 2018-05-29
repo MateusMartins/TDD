311f84eb7afc11d0a3f6034f79c4176ad5a01544
Mateus Ferreira <mateusmferreira@live.com>
2018-05-29 11:00:22 -0300

    Testes unitários

--- lists/tests.py
+++ lists/tests.py
@@ -3,10 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.http import HttpResponse
 from lists.views import home_page
-
-
-
-
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
@@ -16,10 +13,10 @@ class HomePageTest(TestCase):
 
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
 
+        self.assertTemplateUsed(response, 'home.html')
